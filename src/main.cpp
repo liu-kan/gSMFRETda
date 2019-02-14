0c52e6178499715aae173aaaf932e4ab387d5cd5
Kan Liu <liuk@126.com>
2019-02-14 18:29:42 +0800

    set params

--- src/main.cpp
+++ src/main.cpp
@@ -17,20 +17,20 @@ parse(int argc, char* argv[])
     options
       .allow_unrecognised_options()
       .add_options()
-      ("i,input", "Input HDF5", cxxopts::value<std::string>())
+      ("i,input", "Input HDF5", cxxopts::value<string>())
     ;
 
     auto result = options.parse(argc, argv);    
     if (result.count("input")<1)
     {
-        std::cout << "Input file of HDF5 arg -i/--input must be assigned " << std::endl;
+        cout << "Input file of HDF5 arg -i/--input must be assigned " << endl;
         exit(1);
     }
     return result;
 
   } catch (const cxxopts::OptionException& e)
   {
-    std::cout << "error parsing options: " << e.what() << std::endl;
+    cout << "error parsing options: " << e.what() << endl;
     exit(1);
   }
 }
@@ -39,19 +39,28 @@ int main(int argc, char* argv[])
 {
     auto result = parse(argc, argv);
     // auto arguments = result.arguments();
-    // std::cout << "Saw " << arguments[0].key() << " arguments" << std::endl;
-    std::string H5FILE_NAME=result["input"].as<std::string>();    
-    std::vector<uint32_t> istart;std::vector<uint32_t> istop;    
-    std::vector<int64_t> stop;std::vector<int64_t> start;
-    std::vector<int64_t> times_ms;
-    std::vector<unsigned char> mask_ad;std::vector<unsigned char> mask_dd;
-    std::vector<float> T_burst_duration;std::vector<float> SgDivSr;
+    // cout << "Saw " << arguments[0].key() << " arguments" << endl;
+    string H5FILE_NAME=result["input"].as<string>();    
+    vector<uint32_t> istart;vector<uint32_t> istop;    
+    vector<int64_t> stop;vector<int64_t> start;
+    vector<int64_t> times_ms;
+    vector<unsigned char> mask_ad;vector<unsigned char> mask_dd;
+    vector<float> T_burst_duration;vector<float> SgDivSr;
     float clk_p,bg_ad_rate,bg_dd_rate;
     loadhdf5(H5FILE_NAME,start,stop,istart,istop,times_ms,mask_ad,mask_dd,T_burst_duration,
         SgDivSr,clk_p,bg_ad_rate,bg_dd_rate);
-    // std::cout << "mask_ad len " << mask_ad.size() << " mask_ad last " << mask_ad[34367292]<< std::endl;
+    // cout << "mask_ad len " << mask_ad.size() << " mask_ad last " << mask_ad[34367292]<< endl;
     assert (mask_ad.size() == times_ms.size());
-    cout<<times_ms.size()<<endl;
-    mc_gpu(mask_ad,mask_dd);
+    assert (T_burst_duration.size() == SgDivSr.size());
+    assert (T_burst_duration.size() == istart.size());
+    cout<<times_ms.size()<<endl;    
+    mc pdamc(0);
+    pdamc.init_data_gpu(start,stop,istart,istop,times_ms,mask_ad,mask_dd,T_burst_duration,
+         SgDivSr,clk_p,bg_ad_rate,bg_dd_rate);
+    vector<float> args={0.2,0.3,0.4,1,2,3,4,5,6,0.9,0.9,0.9};
+    for (int i=0;i<args.size();i++)cout<< args[i];cout<<endl;
+    pdamc.set_params(3,args);
+    cout<<pdamc.eargs<<endl;
+    cout<<pdamc.vargs<<endl;
     return 0;   
 }
--- src/mc.hpp
+++ src/mc.hpp
@@ -1,6 +1,34 @@
 #ifndef mc_HPP_INCLUDED
 #define mc_HPP_INCLUDED
 #include <vector>
-void mc_gpu(std::vector<unsigned char> mask_ad,std::vector<unsigned char> mask_dd);
+#include <cstdint>
+using namespace std;
+#include "eigenhelper.hpp"
+class mc
+{
+    protected:
+        int devid;
+        long sz_tag;
+        unsigned char *g_mask_ad,*g_mask_dd;
+        float *gchi2,*hchi2;        
+        int sz_burst;
+        int64_t *g_start,*g_stop,*g_times_ms;
+        uint32_t *g_istart,*g_istop;   
+        float *g_burst_duration,*g_SgDivSr,clk_p,bg_ad_rate,bg_dd_rate;
+        MatrixXf *matK,*matP;
+    public:
+        RowVectorXf eargs,vargs,kargs;
+        void free_data_gpu();
+        void run_kernel();
+        void init_data_gpu(vector<int64_t>& start,vector<int64_t>& stop,
+            vector<uint32_t>& istart,vector<uint32_t>& istop,
+            vector<int64_t>& times_ms,
+            vector<unsigned char>& mask_ad,vector<unsigned char>& mask_dd,
+            vector<float>& T_burst_duration,vector<float>& SgDivSr,
+            float& clk_p,float& bg_ad_rate,float& bg_dd_rate);
+        ~mc();
+        mc(int devid);
+        bool set_params(int n,vector<float>& args);
+};
 
 #endif
