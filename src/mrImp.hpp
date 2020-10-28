c93c452f39c4887bc7429d7d7f482edb637d9fa2
liukan <liuk@wipm.ac.cn>
2020-10-28 11:31:53 +0800

    remove deps on rmm, move serv_py to third part smFRETLikelihood

--- src/mrImp.hpp
+++ src/mrImp.hpp
@@ -1,20 +1,11 @@
 #pragma once
-
-#include <rmm/mr/device/pool_memory_resource.hpp>
-#include "rmm/detail/error.hpp"
-#include "rmm/mr/device/cuda_memory_resource.hpp"
-#include "rmm/mr/device/default_memory_resource.hpp"
-
-
+#include "cuda_tools.hpp"
+#include "helper_cuda.h"
 class mrImp
 {
-    using Pool = rmm::mr::pool_memory_resource<rmm::mr::device_memory_resource>;
     protected:
-        Pool *mr;
         bool _sync;
         int _gpuid;
-        rmm::mr::cuda_memory_resource *cuda_mr;
-        rmm::mr::pool_memory_resource<rmm::mr::cuda_memory_resource> *pool_mr;
         int _type;
     public:
         mrImp(std::size_t init_size,float maxe,int gpuid=0, bool sync=false,int type=0);
