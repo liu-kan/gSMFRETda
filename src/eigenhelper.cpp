6e285c28f1a6c4761873dab9a46438c3fd065cfd
Kan Liu <liukan@126.com>
2020-12-01 18:04:22 +0800

    First windows version can run.

--- src/eigenhelper.cpp
+++ src/eigenhelper.cpp
@@ -1,42 +1,39 @@
 #include "eigenhelper.hpp"
 #include <iostream>
-using namespace std;
-using namespace Eigen;
-bool genMatK(arrFF** matK,int n, RowVectorXf& args){
+
+bool genMatK(float* matK,int n, RowVectorXf& args){
     if (args.size()<1)
         return false;
     if (args.size()!=n*n-n)
         return false;
-    // if (*matK!=NULL)
-        delete(*matK);
-    *matK=new arrFF(n,n);
     for (int i=0;i<n;i++)
         for (int j=0;j<n;j++)
             if (i<j)
-                (**matK)(i,j)=args[i*(n-1)+j-1];
+                *(matK+i+j*n)=args[i*(n-1)+j-1];
             else if(i>j)
-                (**matK)(i,j)=args[i*(n-1)+j];
+                *(matK+i+j*n)=args[i*(n-1)+j];
             else
-                (**matK)(i,j)=0;
-    for (int i=0;i<n;i++)
-        for (int j=0;j<n;j++)
-            if (i==j)
-                (**matK)(i,j)=-(*matK)->col(j).sum();                   
+                *(matK+i+j*n)=0;
+    for(int j=0;j<n;j++)
+        for (int ii=0;ii<n;ii++)
+            if(ii!=j)
+                *(matK+j+j*n)-=*(matK+ii+j*n);
     return true;
 }
 
-bool genMatP(arrF** matP,arrFF* matK){
-    int n=matK->rows();
+bool genMatP(float* matP,float* matK,int n){
+    // int n=matK->rows();
     if (n<1)
         return false;
-    // if (*matP!=NULL)
-        delete(*matP);        
-    *matP=new arrF(n);
     float ap=0;
-    for (int i=0;i<n;i++) 
-                (**matP)(i)=(*matK).row(i).sum()-(*matK)(i,i);
-    ap=(**matP).sum();
-    for (int i=0;i<n;i++) 
-        (**matP)(i)=(**matP)(i)/ap;
+    for (int i=0;i<n;i++){
+        *(matP+i)=0;
+        for(int j=0;j<n;j++)
+                *(matP+i)+=*(matK+i+j*n);
+        *(matP+i)-=*(matK+i+i*n);
+        ap+=*(matP+i);
+    }
+    // for (int i=0;i<n;i++) 
+    //     *(*matP+i)=*(*matP+i)/ap;
     return true;
 }
