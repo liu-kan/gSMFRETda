89bec9e6afacbc8739cd5e8db20703ffe07c5a36
liu-kan <liukan@126.com>
2020-12-02 11:46:19 +0800

    exit wapper gracefully

--- buildWinDebug.bat
+++ buildWinDebug.bat
@@ -6,4 +6,4 @@ SET srcpath=%~dp0
 set src_dir=%srcpath:~0,-1%
 conan install %src_dir% --profile %src_dir%\conan_debug_profile.txt --build=missing
 cmake %src_dir% -G "Visual Studio 16"
-cmake --build . --config Debug
+cmake --build . --config Debug -- /p:CharacterSet=Unicode
